6c032714d402c99bc75a0040f3f3679f2b8ea866
Mark Bakery <mr.jiadewang@gmail.com>
2022-06-19 15:13:15 +0800

    temp

--- django_api/backend/settings.py
+++ django_api/backend/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER':os.getenv("DB_USERNAME"),
         'PASSWORD':os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"), 
+        'HOST': os.getenv("DB_HOST"),
         'PORT':os.getenv("DB_PORT"),
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'; "
